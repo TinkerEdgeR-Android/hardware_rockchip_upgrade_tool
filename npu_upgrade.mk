44f3cce8448e10985704b31eb771b3e3c5942ef8
Weixin Zhou <zwx@rock-chips.com>
2018-09-27 17:05:04 +0800

    add upgrade tool
    Change-Id: I4521b35eede30891302a610b27a2486f31ca9a9f
    Signed-off-by: Weixin Zhou <zwx@rock-chips.com>

---
+++ npu_upgrade.mk
@@ -0,0 +1,8 @@
+PRODUCT_COPY_FILES += \
+		      hardware/rockchip/upgrade_tool/npu_upgrade:vendor/bin/npu_upgrade \
+		      hardware/rockchip/upgrade_tool/upgrade_tool:vendor/bin/upgrade_tool \
+		      hardware/rockchip/upgrade_tool/npu_upgrade.rc:vendor/etc/init/npu_upgrade.rc \
+		      hardware/rockchip/upgrade_tool/npu_fw/MiniLoaderAll.bin:vendor/etc/firmware/npu_fw/MiniLoaderAll.bin \
+		      hardware/rockchip/upgrade_tool/npu_fw/trust.img:vendor/etc/firmware/npu_fw/trust.img \
+		      hardware/rockchip/upgrade_tool/npu_fw/uboot.img:vendor/etc/firmware/npu_fw/uboot.img \
+		      hardware/rockchip/upgrade_tool/npu_fw/boot.img:vendor/etc/firmware/npu_fw/boot.img \
