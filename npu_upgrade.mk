75c3a72bff8c653c46abe42800637724dcc072f9
hzf <hzf@rock-chips.com>
2019-01-28 15:33:59 +0800

    npu_upgrade: fix compilation errors on Android N.
    Change-Id: I59650ca6d9d7eb9d2ca1a97157abcc6a5afbed7e
    Signed-off-by: Bian Jinchen <kenjc.bian@rock-chips.com>

--- npu_upgrade.mk
+++ npu_upgrade.mk
@@ -1,16 +1,16 @@
 ifeq (1,$(strip $(shell expr $(PLATFORM_VERSION) \>= 9)))
 PRODUCT_COPY_FILES += \
-    hardware/rockchip/upgrade_tool/npu_upgrade_pie.rc:vendor/etc/init/npu_upgrade.rc
+    hardware/rockchip/upgrade_tool/npu_upgrade_pie.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/npu_upgrade.rc
 else
 PRODUCT_COPY_FILES += \
-    hardware/rockchip/upgrade_tool/npu_upgrade.rc:vendor/etc/init/npu_upgrade.rc
+    hardware/rockchip/upgrade_tool/npu_upgrade.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/npu_upgrade.rc
 endif
 
 PRODUCT_COPY_FILES += \
-    hardware/rockchip/upgrade_tool/npu_upgrade:vendor/bin/npu_upgrade \
-    hardware/rockchip/upgrade_tool/upgrade_tool:vendor/bin/upgrade_tool \
-    hardware/rockchip/upgrade_tool/npu_fw/MiniLoaderAll.bin:vendor/etc/firmware/npu_fw/MiniLoaderAll.bin \
-    hardware/rockchip/upgrade_tool/npu_fw/trust.img:vendor/etc/firmware/npu_fw/trust.img \
-    hardware/rockchip/upgrade_tool/npu_fw/uboot.img:vendor/etc/firmware/npu_fw/uboot.img \
-    hardware/rockchip/upgrade_tool/npu_fw/boot.img:vendor/etc/firmware/npu_fw/boot.img \
-    hardware/rockchip/upgrade_tool/npu_fw/npu_commit_id.xml:vendor/etc/firmware/npu_fw/npu_commit_id.xml \
+    hardware/rockchip/upgrade_tool/npu_upgrade:$(TARGET_COPY_OUT_VENDOR)/bin/npu_upgrade \
+    hardware/rockchip/upgrade_tool/upgrade_tool:$(TARGET_COPY_OUT_VENDOR)/bin/upgrade_tool \
+    hardware/rockchip/upgrade_tool/npu_fw/MiniLoaderAll.bin:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/npu_fw/MiniLoaderAll.bin \
+    hardware/rockchip/upgrade_tool/npu_fw/trust.img:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/npu_fw/trust.img \
+    hardware/rockchip/upgrade_tool/npu_fw/uboot.img:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/npu_fw/uboot.img \
+    hardware/rockchip/upgrade_tool/npu_fw/boot.img:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/npu_fw/boot.img \
+    hardware/rockchip/upgrade_tool/npu_fw/npu_commit_id.xml:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/npu_fw/npu_commit_id.xml \
