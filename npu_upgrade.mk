2293c6e1ece8d38e276f680c2414034c19d5698f
hzf <hzf@rock-chips.com>
2019-03-12 17:24:24 +0800

    change npu_fw path on vendor/etc/npu_fw/
    Change-Id: I9fecf87d0cd7f4b54659ccbde5956aeb9124fac9

--- npu_upgrade.mk
+++ npu_upgrade.mk
@@ -9,8 +9,8 @@ endif
 PRODUCT_COPY_FILES += \
     hardware/rockchip/upgrade_tool/npu_upgrade:$(TARGET_COPY_OUT_VENDOR)/bin/npu_upgrade \
     hardware/rockchip/upgrade_tool/upgrade_tool:$(TARGET_COPY_OUT_VENDOR)/bin/upgrade_tool \
-    hardware/rockchip/upgrade_tool/npu_fw/MiniLoaderAll.bin:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/npu_fw/MiniLoaderAll.bin \
-    hardware/rockchip/upgrade_tool/npu_fw/trust.img:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/npu_fw/trust.img \
-    hardware/rockchip/upgrade_tool/npu_fw/uboot.img:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/npu_fw/uboot.img \
-    hardware/rockchip/upgrade_tool/npu_fw/boot.img:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/npu_fw/boot.img \
-    hardware/rockchip/upgrade_tool/npu_fw/npu_commit_id.xml:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/npu_fw/npu_commit_id.xml \
+    hardware/rockchip/upgrade_tool/npu_fw/MiniLoaderAll.bin:$(TARGET_COPY_OUT_VENDOR)/etc/npu_fw/MiniLoaderAll.bin \
+    hardware/rockchip/upgrade_tool/npu_fw/trust.img:$(TARGET_COPY_OUT_VENDOR)/etc/npu_fw/trust.img \
+    hardware/rockchip/upgrade_tool/npu_fw/uboot.img:$(TARGET_COPY_OUT_VENDOR)/etc/npu_fw/uboot.img \
+    hardware/rockchip/upgrade_tool/npu_fw/boot.img:$(TARGET_COPY_OUT_VENDOR)/etc/npu_fw/boot.img \
+    hardware/rockchip/upgrade_tool/npu_fw/npu_commit_id.xml:$(TARGET_COPY_OUT_VENDOR)/etc/npu_fw/npu_commit_id.xml \
