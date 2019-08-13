171c27face861bf4e1f908f26ba1f21e3f77093a
Weixin Zhou <zwx@rock-chips.com>
2019-08-13 09:44:30 +0800

    npu_pcie_fw: 	rknn_server: 0.9.9 (8509ec7c) 	npu transfer: 1.9.7 	librknn_runtime: 1.1.0 (e18060b) 	galcore.ko (6.4.0, 67399c4)
    	local patch:
    		https://10.10.10.29/#/c/linux/external/rkscript/+/81853/
    		https://10.10.10.29/#/c/rk/kernel/+/81929/
    
    Change-Id: Ieffb8541a0cd24ad2401968614f12401ec7c3a16

--- npu_upgrade.mk
+++ npu_upgrade.mk
@@ -7,20 +7,23 @@ PRODUCT_COPY_FILES += \
 endif
 
 PRODUCT_COPY_FILES += \
-    hardware/rockchip/upgrade_tool/upgrade_tool:$(TARGET_COPY_OUT_VENDOR)/bin/upgrade_tool \
-    hardware/rockchip/upgrade_tool/npu_fw/MiniLoaderAll.bin:$(TARGET_COPY_OUT_VENDOR)/etc/npu_fw/MiniLoaderAll.bin \
-    hardware/rockchip/upgrade_tool/npu_fw/trust.img:$(TARGET_COPY_OUT_VENDOR)/etc/npu_fw/trust.img \
-    hardware/rockchip/upgrade_tool/npu_fw/uboot.img:$(TARGET_COPY_OUT_VENDOR)/etc/npu_fw/uboot.img \
+    hardware/rockchip/upgrade_tool/upgrade_tool:$(TARGET_COPY_OUT_VENDOR)/bin/upgrade_tool
 
 #ifeq ($(strip $(BOARD_NPU_WITH_PCIE)),true)
 ifeq ($(BOARD_NPU_WITH_PCIE),true)
 PRODUCT_COPY_FILES += \
     hardware/rockchip/upgrade_tool/npu_upgrade_pcie:$(TARGET_COPY_OUT_VENDOR)/bin/npu_upgrade \
-    hardware/rockchip/upgrade_tool/npu_fw/boot-pcie.img:$(TARGET_COPY_OUT_VENDOR)/etc/npu_fw/boot.img \
-    hardware/rockchip/upgrade_tool/npu_fw/npu_pcie_commit_id.xml:$(TARGET_COPY_OUT_VENDOR)/etc/npu_fw/npu_commit_id.xml
+    hardware/rockchip/upgrade_tool/npu_pcie_fw/MiniLoaderAll.bin:$(TARGET_COPY_OUT_VENDOR)/etc/npu_fw/MiniLoaderAll.bin \
+    hardware/rockchip/upgrade_tool/npu_pcie_fw/boot.img:$(TARGET_COPY_OUT_VENDOR)/etc/npu_fw/boot.img \
+    hardware/rockchip/upgrade_tool/npu_pcie_fw/trust.img:$(TARGET_COPY_OUT_VENDOR)/etc/npu_fw/trust.img \
+    hardware/rockchip/upgrade_tool/npu_pcie_fw/uboot.img:$(TARGET_COPY_OUT_VENDOR)/etc/npu_fw/uboot.img \
+    hardware/rockchip/upgrade_tool/npu_pcie_fw/npu_commit_id.xml:$(TARGET_COPY_OUT_VENDOR)/etc/npu_fw/npu_commit_id.xml
 else
 PRODUCT_COPY_FILES += \
     hardware/rockchip/upgrade_tool/npu_upgrade:$(TARGET_COPY_OUT_VENDOR)/bin/npu_upgrade \
+    hardware/rockchip/upgrade_tool/npu_fw/MiniLoaderAll.bin:$(TARGET_COPY_OUT_VENDOR)/etc/npu_fw/MiniLoaderAll.bin \
     hardware/rockchip/upgrade_tool/npu_fw/boot.img:$(TARGET_COPY_OUT_VENDOR)/etc/npu_fw/boot.img \
+    hardware/rockchip/upgrade_tool/npu_fw/trust.img:$(TARGET_COPY_OUT_VENDOR)/etc/npu_fw/trust.img \
+    hardware/rockchip/upgrade_tool/npu_fw/uboot.img:$(TARGET_COPY_OUT_VENDOR)/etc/npu_fw/uboot.img \
     hardware/rockchip/upgrade_tool/npu_fw/npu_commit_id.xml:$(TARGET_COPY_OUT_VENDOR)/etc/npu_fw/npu_commit_id.xml
 endif
